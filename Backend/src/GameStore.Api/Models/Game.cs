b98d026fa31b809523c8ab262dc9e8a6f6faa423
tanviriqbal <tanviriqbal4u@live.com>
2026-05-29 19:54:34 +1000

    Implemented GET endpoint

---
+++ Backend/src/GameStore.Api/Models/Game.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace GameStore.Api.Models
+{
+    public class Game
+    {
+        public Guid Id { get; set; }
+        public required string Name { get; set; }
+        public required string Genre { get; set; }
+        public decimal Price { get; set; }
+        public DateOnly ReleaseDate { get; set; } 
+    }
+}
--- Backend/src/GameStore.Api/Program.cs
+++ Backend/src/GameStore.Api/Program.cs
@@ -1,6 +1,44 @@
+using GameStore.Api.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+List<Game> games = new List<Game>
+{
+    new Game
+    {
+        Id = Guid.NewGuid(),
+        Name = "The Witcher 3: Wild Hunt",
+        Genre = "RPG",
+        Price = 59.99m,
+        ReleaseDate = new DateOnly(2015, 5, 19)
+    },
+    new Game
+    {
+        Id = Guid.NewGuid(),
+        Name = "Elden Ring",
+        Genre = "Action RPG",
+        Price = 59.99m,
+        ReleaseDate = new DateOnly(2022, 2, 25)
+    },
+    new Game
+    {
+        Id = Guid.NewGuid(),
+        Name = "Cyberpunk 2077",
+        Genre = "Action RPG",
+        Price = 59.99m,
+        ReleaseDate = new DateOnly(2020, 12, 10)
+    }
+};
+
+//GET /games
+app.MapGet("/games", () => games);
+//GET /games/{id}
+app.MapGet("/games/{id}", (Guid id) =>
+{
+    var game = games.Find(g => g.Id == id);
+    return game is not null ? Results.Ok(game) : Results.NotFound();
+});
+//app.MapGet("/", () => "Hello World!");
 
 app.Run();
